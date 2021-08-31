b8c05c0d44431a95f0b17a4ff9a8d6b4cfaa54d9
ye7ia10 <yehiaelsayed234@gmail.com>
2021-08-31 21:02:14 +0200

    start with styling and bootstrap

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ ruby '2.7.4'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.3'
+
+
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
@@ -21,6 +23,8 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+
+
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/packs/packs/application.js
+++ app/packs/packs/application.js
@@ -8,6 +8,7 @@
 // layout file, like app/views/layouts/application.html.erb
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -15,4 +16,3 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
