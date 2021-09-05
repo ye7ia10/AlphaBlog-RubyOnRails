bdbecdea67b4779e7f7dd2e6651d2594e79f2016
ye7ia10 <yehiaelsayed234@gmail.com>
2021-09-05 16:49:21 +0200

    add pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,9 @@ class ArticlesController < ApplicationController
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.all
+    # @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)  #pagination
+
   end
 
   # GET /articles/1 or /articles/1.json
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,8 @@ class UsersController < ApplicationController
     end
 
     def index
-        @users = User.all
+        # @users = User.all
+        @users =  User.paginate(page: params[:page], per_page: 5)  #pagination
     end
 
     def show
